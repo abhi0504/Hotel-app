9fdec675eea16b4eeac239ed45138f77e1d00ca3
Abhishek Sharma <sharmaash876@gmail.com>
2020-10-14 19:05:24 +0530

    data added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Switch , Route } from 'react-router-dom';
 import Navbar from './Components/Navbar'
 
+import Home from './pages/home'
+import Rooms from './pages/rooms'
 
 function App() {
   
   return (
     <div>
       <Navbar />
-      Hi Guys
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+      </Switch>
     </div>
   );
 }
